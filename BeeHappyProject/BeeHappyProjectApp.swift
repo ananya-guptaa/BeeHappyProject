5b6924622d8ab93e60104869a349fba00e6a29b8
codermaste <109619670+codermaste@users.noreply.github.com>
2023-06-28 17:35:34 -0400

    Initial Commit

---
+++ BeeHappyProject/BeeHappyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeeHappyProjectApp.swift
+//  BeeHappyProject
+//
+//  Created by Ananya Gupta on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct BeeHappyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
